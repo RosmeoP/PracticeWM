0aa6fdb95f1d8f160b0ca5ee30b396eacc4bf357
moisescastro13 <moycastro3@hotmail.es>
2023-08-18 21:38:28 -0600

    Add sumNumbs using reduce function

--- js/idk.js
+++ js/idk.js
@@ -1,4 +1,4 @@
-const numero = [1,2,3,4,5,6,7]
+const numero = [1, 2, 3, 4, 5, 6, 7];
 /*
 // agrega número a un array
 numero.push(8, 9) 
@@ -20,50 +20,51 @@ arr.unshift(0); // [0, 1, 2, 3, 4, 5]
 console.log(arr)
 */
 
-numero.unshift(0)
+numero.unshift(0);
 
-console.log(numero)
+console.log(numero);
 
 //shift lo elimina
-numero.shift(0)
+numero.shift(0);
 
-console.log(numero)
-
+console.log(numero);
 
 //array original
 const arr = [0, 2, 3, 4, 5, 10];
 
-arr.includes(5); 
+arr.includes(5);
 
-console.log(arr.includes(5))
+console.log(arr.includes(5));
 
 //Retorna el valor del índice que le pasamos. indexOf
 
-arr.indexOf(10)
+arr.indexOf(10);
 
-console.log(arr.indexOf(10))
+console.log(arr.indexOf(10));
 
 //Retornan un string de un array
 
+let arrtoString = arr.toString();
 
-let arrtoString = arr.toString()
-
-console.log(arrtoString)
+console.log(arrtoString);
 
 //slice() toma dos parámetros. Un parámetro es el índice inicial y el otro es el final.
 
-let numbs = [1, 10, 100, 1000, 10000]
+let numbs = [1, 10, 100, 1000, 10000];
 
-let newNumbs = numbs.slice(0,3)
+let newNumbs = numbs.slice(0, 3);
 
 console.log(newNumbs);
+
+const sumNumbs = numbs.reduce((acc, ele) => acc + ele * 2, 0);
+console.log(sumNumbs);
 
 //splice() toma el índice inicial y el número de elementos a partir del índice inicial, sino se coloca el número de elementos, se tomará todo el resto del array a partir del índice inicial.
 //pslice puede modificar el array original
 
-let newNumbs1 = numbs.splice(0,2)
+let newNumbs1 = numbs.splice(0, 2);
 
-console.log(newNumbs1)
+console.log(newNumbs1);
 /* 
 let x = [1,2,3,4,5]
 
@@ -72,10 +73,10 @@ x.reverse();
 
 console.log(x)
 */
-x = numbs.concat(arr)
+x = numbs.concat(arr);
 
-console.log(x)
+console.log(x);
 
 let y = x.reverse();
 
-console.log(y)
+console.log(y);
